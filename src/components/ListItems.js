1ece0280bed13ec67350b7516cb9e34cfd255740
aaroncaraway <aaroncaraway42@gmail.com>
2020-11-22 16:45:10 -0800

    Add call to db

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
 import ImgMediaCard from './ImgMediaCard'
 
 
 const ListItems = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState({ recipes: []})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://lettuceshop-api.herokuapp.com/'
+      );
+      console.log(result.data)
+      setData({ recipes: result.data })
+      setLoading(false)
+    };
+    fetchData();
+  }, [])
+
   return(
     <>
     <div className="listitems">ListItems</div>
+    {loading ?
+    <h1>loading</h1>
+    :     <div>{data.recipes && data.recipes.map(item => {
+      return(
+      <ul>
+        <li key={item.recipe_id}>
+          <a href={item.recipeInstructions_url}>{item.name}</a>
+        </li>
+      </ul>
+      )
+    })}</div>}
+
     <ImgMediaCard />
     </>
 
